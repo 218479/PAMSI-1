0d3dbf782f9be6640f713ab4663f58282d075dea
218479 <218479@student.pwr.edu.pl>
2016-03-10 18:28:05 +0100

    Dokonanie drobnej zmiany w menu

--- Lab2/src/main.cpp
+++ Lab2/src/main.cpp
@@ -8,22 +8,24 @@ int main()
 {
     int wybor=99;
     int ile_liczb,o_ile;
-    int alg1=1, alg2=100;
     double trwanie;
     time_t start, stop;
-    cout<<"Ile dodatkowych liczba ma zostac dopisanych do tablicy?"<<endl;
+    cout<<"Ile liczba ma zostac dopisanych do tablicy?"<<endl;
     cin>>ile_liczb;
+    cout<<"\n\nWybierz algorytm:\n1.Powiekszanie o 1\n2.Powiekszanie o 100"<<endl;
+    cout<<"3.Powiekszenie o zadana wczesniej liczb"<<endl;
+    cout<<"0.Wyjscie z programu\n"<<endl;
     while(wybor!=0)
     {
-    cout<<"\n\nWybierz algorytm:\n1.Powiekszanie o 1\n2.Powiekszanie o 100\n3.Powiekszenie o zadana wczesniej liczb\n0.Wyjscie z programu\n";
+
         cin>>wybor;
         switch(wybor)
         {
         case 1:
-            o_ile=alg1;
+            o_ile=1;
             break;
         case 2:
-            o_ile=alg2;
+            o_ile=100;
             break;
         case 3:
             o_ile=ile_liczb;
